59a153d46754c3cda94ccb70636b469a215dbb5b
xovrashok <juliaruda033@gmail.com>
2026-07-28 16:11:19 +0300

    fix(vite): import resolve in vite config for multi-page build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import injectHTML from "vite-plugin-html-inject";
 
 export default defineConfig({
   base: "./",
   plugins: [injectHTML()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        watchlist: resolve(__dirname, "my-watchlist.html"),
+      },
+    },
+  },
 });
